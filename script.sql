0c2c80499b16237fa006ea756202327894cb722f
HariharanAI <hariharansv1810@gmail.com>
2025-02-19 14:01:28 +0530

    Initial commit for dockerized MySQL, Node.js, and React.js app

--- script.sql
+++ script.sql
@@ -1,14 +1,13 @@
--- Create the appdb database
+-- Create the appdb database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS appdb;
 
 -- Use the appdb database
 USE appdb;
 
--- Create the apptb table
-CREATE TABLE `appdb`.`apptb` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
-
-
+-- Create the apptb table if it doesn't exist
+CREATE TABLE IF NOT EXISTS apptb (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
 
